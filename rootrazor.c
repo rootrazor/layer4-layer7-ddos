205178409383b9af52c7dd6c3224e6c4c78cda00
rootrazor <61475920+rootrazor@users.noreply.github.com>
2020-03-16 11:46:16 +0300

    Update rootrazor.c

--- rootrazor.c
+++ rootrazor.c
@@ -7,6 +7,31 @@
 #define mavi "\033[0;36m"
 #define sari  "\x1B[33m"
 int main(int argc, char **argv[]){
+
+echo -e "     .--. " | lolcat -a -d 2
+echo -e "    |o_o |	 " | lolcat -a -d 2
+echo -e "    |:_/ |   ========= DDoS Sistemi v1.0 ========= " | lolcat -a -d 2
+echo -e "  //   \ \  ====== Code by RootRazor ====== " | lolcat -a -d 2
+echo -e " (|     | ) ====== Thanks to Turkharekat ======= " | lolcat -a -d 2
+echo -e " /\'\_   _/ " | lolcat -a -d 2
+echo -e " \___)=(___/ " | lolcat -a -d 2
+
+ 
+# --- verilerim ---
+verilerim() { 
+	echo -e "\n- bu script rootrazor tarafindan kodlamistir C ve BASH olarak."  | lolcat -a -d 10
+	echo -e "\n- razorscript.c bunu indiriniz"  | lolcat -a -d 10
+	echo -e "\n- yukardaki scripti derlemeden kullanamazsiniz."  | lolcat -a -d 10
+	echo -e "\n- Birden Fazla Method ve Katmanlarimiz Sayesinde Atak Cikarabilirsiniz ."  | lolcat -a -d 10
+	echo -e "\nOrnek Kullanim:\n - ./rootrazor İP PORT tcp, udp, amp, syn, ack" | lolcat -a -d 2
+	} 
+
+
+if [ $# -lt 4 ] 
+then
+	verilerim
+	exit 1
+fi 
  
 char aktifet[255];
 printf("%s• •        Kodlama: RootRaz0r • •\n",sari,mavi,kirmizi,yesil,kirmizi,yesil,kirmizi,sari);
